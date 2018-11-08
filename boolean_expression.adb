f55b06f48b041b9bb8f90174f9686cf89c1b04c9
Zeke <zekelawal@gmail.com>
2018-11-07 23:27:57 -0500

    boolean_expression

--- boolean_expression.adb
+++ boolean_expression.adb
@@ -1,4 +1,51 @@
-procedure boolean_expression is
-begin
+with Ada.Characters.Handling; use Ada.Characters.Handling
+  
+package body boolean_expression is
    
-end boolean_expression;
+   function boolean_expression(op: in relative_operator, expr1: in arithmetic_expression, epr2: in arithmetic_expression) return Binary_Expression is 
+     
+      BE: boolean_expression;
+      
+   begin
+      
+      if op == null then
+         raise Data_Error => Put("Relative Operator is null");
+      elsif expr1 == null || expr2 == null then
+         raise Data_Error => Put("Arithmetic Expression is null");
+      end if
+           
+      BE.op := op;
+      BE.expr1 := expr1;
+      BE.expr2 := expr2;
+      return BE;
+    end boolean_expression;
+  
+  function evaluate () return Boolean is
+      
+    begin
+      reult: Boolean;
+      
+      case BE.op is
+         when eq_operator => 
+            if BE.expr1.evaluate() == BE.expr2.evaluate() then
+               result := True;
+         when ne_operator => 
+            if expr1.evaluate() != expr2.evaluate() then
+               result := True;
+         when lt_operator => 
+            if expr1.evaluate() != expr2.evaluate() then
+               result := True;
+         when le_operator => 
+            if expr1.evaluate() != expr2.evaluate() then
+               result := True;
+         when gt_operator => 
+            if expr1.evaluate() != expr2.evaluate() then
+               result := True;
+         when ge_operator => 
+            if expr1.evaluate() != expr2.evaluate() then
+                  result := True;
+         when others => raise Error;              
+      end case;               
+      end evaluate;   
+      
+end BinaryExpression;
