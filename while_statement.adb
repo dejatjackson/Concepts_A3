6316042216138ea738e146e3cb66148eeba37a10
Deja Jackson <dejatjackson@gmail.com>
2018-11-08 10:49:41 -0500

    Update while_statement.adb

--- while_statement.adb
+++ while_statement.adb
@@ -1,4 +1,23 @@
-procedure while_statement is
-begin
+package body while_statement is
+function create_wstat(E: in B_Expression, B: in blk) return W_Stat is
+    
+      WS: W_stat;
+      
+    begin
+      if(b_expr == null)
+        //TODO
+      if(blk == null)
+        //TODO
+         WS.b_expr = b_expr;
+         WS.blk = blk;
+     return WS;
+    end create_wstat
+   
+   
+    procedure excecute(W: in W_Stat) //TODO:fix this method
+     begin 
+      while(b_expr.eval())
+          blk.process();
+     end execute;
    
 end while_statement;
