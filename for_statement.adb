a0202b0b6262319429b1735eca0465cc67de02cb
Deja Jackson <dejatjackson@gmail.com>
2018-11-08 10:53:47 -0500

    Update for_statement.adb

--- for_statement.adb
+++ for_statement.adb
@@ -1,4 +1,23 @@
-procedure for_statement is
-begin
+package body for_statement is
+   function create_forstat(E: in B_Expression, B: in Blk) return For_Stat is
+    
+      FS: For_stat;
+      
+    begin
+      if(b_expr == null)
+        //TODO
+      if(blk == null)
+        //TODO
+         FS.b_expr = b_expr;
+         FS.blk = blk;
+     return FS;
+    end create_forstat
+   
+   
+    procedure excecute(F: in For_Stat) //TODO:fix this method
+     begin 
+      while(b_expr.eval())
+          blk.process();
+     end
    
 end for_statement;
