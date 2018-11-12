c0ec641a7cc3bc51336ab6e6bf73bff234b52b8f
Deja Jackson <dejatjackson@gmail.com>
2018-11-11 19:23:05 -0500

    Update lexicalanalyzer.adb

--- lexicalanalyzer.adb
+++ lexicalanalyzer.adb
@@ -1,4 +1,40 @@
 package body lexicalAnalyzer is
-begin
-   
+
+   //TODO the constructor
+   function create_lexAnalyzer
+     begin 
+      //TODO
+   end
+   procedure processline() //TODo
+   begin 
+      //TODO
+   end
+   function getTokenType() //TODO
+   begin 
+      //TODO
+   end   
+   function allDigits() //TODO
+   begin 
+      //TODO
+   end function getLexeme() //TODO
+   begin 
+      //TODO
+   end  
+   function skipWhiteSpace() //TODO
+   begin 
+      //TODO
+   end   
+   function getLookaheadToken() //TODO
+   begin 
+      //TODO
+   end  function getNextToken() //TODO
+   begin 
+      //TODO
+   end function getForExpression() //TODO
+   begin 
+      //TODO
+   end function isValidIdentifier() //TODO
+   begin 
+      //TODO
+   end 
 end lexicalAnalyzer;
